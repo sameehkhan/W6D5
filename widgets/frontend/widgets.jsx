b91c2a07f9eca5fd568bcb08d1695e5bb329cc03
sameehkhan <sameehkhan1@gmail.com>
2018-11-30 13:57:40 -0800

    Add bundle.js

--- widgets/frontend/widgets.jsx
+++ widgets/frontend/widgets.jsx
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 document.addEventListener('DOMContentLoaded',() => {
   const root = document.getElementById('root');
   ReactDOM.render(
-    <h1> React is working!</h1>, root
-  );
+    <h1> React is working!</h1>, root);
 });
+
